c7567a59eaf9e07742c00e9700f1440901f19839
1cpswoole <1169246310@qq.com>
2019-04-05 18:09:02 +0800

    '测试1'

---
+++ app/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HUAWEI
+ * Date: 2019/4/5
+ * Time: 17:54
+ */
+namespace app\controller;
+
+class Index{
+
+	public function ceshi(){
+		echo '李春鹏你是谁啊大萨达所的撒的撒';
+	}
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once  "vendor/autoload.php";
+
+use app\controller\Index;
+
+(new Index())->ceshi();
